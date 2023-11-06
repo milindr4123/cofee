ceddc56bd636736ea70905ea2e0761182f310002
milindr4123 <e.foreros@uniandes.edu.co>
2023-11-05 21:10:05 -0500

    add test

--- src/app/coffee/coffee.service.spec.ts
+++ src/app/coffee/coffee.service.spec.ts
@@ -1,12 +1,20 @@
 /* tslint:disable:no-unused-variable */
 
 import { TestBed, async, inject } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { CoffeeService } from './coffee.service';
+import { AppComponent } from '../app.component';
+import { CoffeeListComponent } from './coffee-list/coffee-list.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('Service: Coffee', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [CoffeeService]
+      imports: [RouterTestingModule,HttpClientTestingModule],
+      providers: [CoffeeService],
+      declarations: [
+        AppComponent, CoffeeListComponent
+      ],
     });
   });
 
